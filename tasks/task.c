b1cef34fc700d10f2fc24581e7d08277210d2908
BenLaverack <146448890+BenLaverack@users.noreply.github.com>
2024-01-29 13:16:26 +0000

    working program using command line input

--- tasks/task.c
+++ tasks/task.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int num1, num2;
+int main(int argc, char *argv[]) {
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    if (argc != 3){
+        printf("Usage: <integer> <integer>\n");
+        return 0;
+    }
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    // add the two entered integers together
+    int sum = atoi(argv[1]) + atoi(argv[2]);
+    printf("the result is: %d\n", sum);
 
-    // Calculate and print the sum
-    int sum = num1 + num2;
-    printf("Sum: %d\n", sum);
-
-    return 0; // Exit successfully
+    return 0;
 }
